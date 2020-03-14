e2a2149818cd8028e64864729a41da1038669b35
Eric <outboundflavor@gmail.com>
2020-03-13 22:09:59 -0500

    added serviceWorkers

--- workouts_app_backend/server.js
+++ workouts_app_backend/server.js
@@ -8,17 +8,17 @@ PORT = 3003
 //middleware
 app.use(express.json())
 
-// const whitelist = ['http://localhost:3000']
-// const corsOptions = {
-//     origin: function (origin, callback){
-//         if (whitelist) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed CORS'))
-//         }
-//     }
-// }
-// app.use(cors(corsOptions))
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback){
+        if (whitelist) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
 
 //mongoose
 mongoose.connection.on('error', err => console.log(err.message + 'is Mongod not running?'))
@@ -30,12 +30,11 @@ mongoose.connection.once('open', () => {
     console.log('connected to mongoose...');
 })
 
-
-
 //controllers and routes
 // app.get('/', (req, res) => {
 //     res.send('Hello World')
 // })
+
 const workoutsController = require ('./controllers/workouts.js')
 app.use('/workouts', workoutsController)
 
